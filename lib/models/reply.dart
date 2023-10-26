b55f7bd73298f4cde20f384a62ce96b4c8af4f93
Devanshu Sharma <devanshu1005@gmail.com>
2023-10-26 14:06:31 +0530

    26

--- lib/models/reply.dart
+++ lib/models/reply.dart
@@ -1,25 +1,24 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:my_project/models/comment.dart';
 
 class Reply {
   final String username;
-  final List<Comment> replies;
+  final String replies;
 
   const Reply({
     required this.username,
     required this.replies,
   });
 
-  static Reply fromSnap(DocumentSnapshot  snap) {
-    var data = snap.data() as Map<String, dynamic>;
+  static Reply fromSnap(Map<String, dynamic> data) {
+    // var data = snap.data() as Map<String, dynamic>;
     return Reply(
-      username: data['username']?? '',
-      replies: data['replies']??'',
+      username: data['name']?? '',
+      replies: data['reply']??'',
     );
   }
 
   Map<String, dynamic> toJson() => {
-        "username": username,
-        "replies": replies,
+        "name": username,
+        "reply": replies,
       };
 }
